8eb8bda3e77ccfa768abb34e504fecf4aa5d8b35
ShamalProg <shamalmalusare2001@gmail.com>
2024-10-11 12:05:18 +0530

    Add files via upload

--- pattern.py
+++ pattern.py
@@ -1,63 +1,129 @@
-# for i in range(0,4):
-#     for j in range(0,i+1):
-#         print("*", end=" ")
+# for i in range(1,5):
+#     for j in range(1,i+1):
+#         print("*",end=" ")
 #     print()
 
-# for i in range(0,6):
-#     num=i+1
-#     for j in range(0,i+1):
-#         print(num,end=" ")
-#     print()
+# for i in range(1,5):
+#     print("*"*i)
+# k=0
+# for i in range(5):
+#     for space in range(1,(5-i)+1):
+#         print(end=" ")
+#     while k!=(2*i-1):
+#         print("* ",end=" ")
+#         k+=1
+#     k=0
+#     print()  
+    
+# for i in range(1,5):   
+#     for j in range(1,i+1):
+#         print(j,end=" ")
+#     print()  
 
-# for i in  range(6+1,0,-1):
-#     for j in range(0,i-1):
-#         print(" *", end=" ")
-#     print()
+#output
+# 1 
+# 1 2 
+# 1 2 3 
+# 1 2 3 4
 
-# for i in range(5+1,0,-1):
-#     num=i-1
-#     for j in range(0,i-1):
-#         print(num,end=" ")
-#     print()
-
+#----------------------------------------------------------
+# num=4
 # for i in range(4,0,-1):
 #     for j in range(0,i):
-#         print(4,end=" ")
+#         print(num,end=" ")
 #     print()
 
-# for i in range(0,7+1):
+#output
+# 4 4 4 4 
+# 4 4 4 
+# 4 4 
+# 4 
+
+#----------------------------------------------------------------
+# for i in range(5,0,-1):
+#     for j in range(1,i+1):
+#         print(i,end=" ")
+#     print()
+# output
+# 5 5 5 5 5 
+# 4 4 4 4
+# 3 3 3
+# 2 2
+# 1
+
+#---------------------------------------------------------------------
+
+# for i in range(7,0,-1):
+#     for j in range(1,i):
+#         print("*",end=" ")
+#     print()
+
+# output
+# * * * * * * 
+# * * * * *
+# * * * *
+# * * *
+# * *
+# *
+
+#-------------------------------------------------------------------
+
+# for i in range(0,7,1):
+#     num=i
+#     for j in range(1,i+1):
+#         print(num,end=" ")
+#     print()
+
+#output
+# 1
+# 2 2
+# 3 3 3
+# 4 4 4 4
+# 5 5 5 5 5
+# 6 6 6 6 6 6
+#-----------------------------------------------------------------
+
+# for i in range(0,7,1):
 #     for j in range(1,i+1):
 #         print(j,end=" ")
 #     print()
 
-# for i in range(0,7+1):
-#     for j in range(i,0,-1):
-#         print(j, end=" ")
+#output
+# 1
+# 1 2
+# 1 2 3
+# 1 2 3 4
+# 1 2 3 4 5
+# 1 2 3 4 5 6
+
+#----------------------------------------------------------------
+
+# for i in range(7,0,-1):
+#     for j in range(0,i+1):
+#         print(j,end=" ")
 #     print()
 
-# for i in range(0,6+1):
-#     for j in range(1,i):
-#         if j%2!=0:
-#             print(j,end=" ")
-#     print()
-
-# for i in  range(6,0,-1):
-#     for j in range(1,i):
-#         print("A",end=" ")
-#     print()
-
-# for i in range(1,6):
-#     for j in range(0,i):
-#         print("A",end=" ")
-#     print()
-            
-# for i in range(0,6+1):
-#     for j in range(1,i):
-#         if j%2==0:
-#             print(j,end=" ")
-#     print()
-
-# for i in range(0,6+1,1):
+#output
+# 0 1 2 3 4 5 6 7 
+# 0 1 2 3 4 5 6
+# 0 1 2 3 4 5
+# 0 1 2 3 4
+# 0 1 2 3
+# 0 1 2
+# 0 1
+#------------------------------------------------------------------
+# for i in range(1,7,1):
 #     for j in range(i,0,-1):
 #         print(j,end=" ")
 #     print()
+
+# output
+# 1 
+# 2 1
+# 3 2 1
+# 4 3 2 1
+# 5 4 3 2 1
+# 6 5 4 3 2 1
+
+#------------------------------------------------------------------
+
